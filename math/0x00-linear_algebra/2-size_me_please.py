7c33d6fd3dcb261003cb86eaebb43f85e8e5deea
afarizap <afarizap@gmail.com>
2021-07-14 12:11:08 -0500

    if name

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from numpy import array
 """ function that returns a matrix shape """
 
 
 def matrix_shape(matrix):
     """ Calculates the shape of a matrix """
     return list(array(matrix).shape)
+
+if __name__ == "__main__":
+    from numpy import array
