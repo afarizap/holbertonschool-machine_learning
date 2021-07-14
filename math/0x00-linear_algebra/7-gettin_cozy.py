d57698bced070157b6c88619ad08112fba0658ac
afarizap <afarizap@gmail.com>
2021-07-14 18:19:19 -0500

    constructing all the matrix again to evade references

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,15 +6,16 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """ concatenates two matrices along a specific axis """
     try:
         new = []
+        for i in mat1:
+            new += [i.copy()]
         if axis == 0:
-            for i in mat1:
-                new += [i.copy()]
             for i in mat2:
-                new += [i.copy()]
+                new += [i]
             return new
         if axis == 1:
-            for i in range(len(mat1)):
-                new += [mat1[i].copy() + mat2[i].copy()]
+            for idx, i in enumerate(mat2):
+                for j in i:
+                    new[idx] += [j]
             return new
     except (ValueError, IndexError):
         return None
