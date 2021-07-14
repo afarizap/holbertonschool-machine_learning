5cfaad5f935f520ccc9f6d427d4764b400222f6d
afarizap <afarizap@gmail.com>
2021-07-14 17:02:47 -0500

    Remove numpy import,  iterate over matrix and op

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-import numpy as np
+"""Across the planes"""
 
 
 def add_matrices2D(mat1, mat2):
     """  adds two matrices element-wise """
-    mat1 = np.array(mat1)
-    mat2 = np.array(mat2)
     try:
-        c = mat1 + mat2
-        return c.tolist()
-    except ValueError:
+        new = []
+        if len(mat1) != len(mat2):
+            return ValueError
+        for i in range(len(mat1)):
+            if len(mat1[i]) != len(mat2[i]):
+                raise ValueError
+            x = []
+            for n in range(len(mat1[i])):
+                t = mat1[i][n] + mat2[i][n]
+                x += [t]
+            new += [x]
+        return new
+    except (ValueError, IndexError):
         return None
