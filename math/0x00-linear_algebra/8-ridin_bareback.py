4a04e61e9fac35848e1d19ae1322e08b9036d285
afarizap <afarizap@gmail.com>
2021-07-14 19:44:44 -0500

    holly multiplication

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
-import numpy as np
+"""Ridin’ Bareback """
 
 
 def mat_mul(mat1, mat2):
     """ performs matrix multiplication """
-    try:
-        return np.matmul(mat1, mat2).tolist()
-    except ValueError:
+    new = []
+    y1 = len(mat1)
+    x1 = len(mat1[0])
+    y2 = len(mat2)
+    x2 = len(mat2[0])
+    if x1 != y2:
         return None
+    # iterate through row of mat1
+    for i in range(y1):
+        y = []
+        # iterate through column of mat2
+        for j in range(x2):
+            x = []
+            # iterate through row of mat2
+            for k in range(y2):
+                # multiply matrix section
+                x += [mat1[i][k] * mat2[k][j]]
+            # sum all the products and add to matrix
+            y += [sum(x)]
+        # add new row to new matrix
+        new += [y]
+    return new
