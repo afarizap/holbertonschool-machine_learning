88d295893274d43bb9c47fecb1ad38b06bcc19fd
afarizap <afarizap@gmail.com>
2021-07-19 10:13:13 -0500

    try advanced

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-import numpy as np
+"""Slice Like A Ninja """
 
 
 def np_slice(matrix, axes={}):
     """ slices a matrix along specific axes """
-    return matrix[3]
+    new = [None] * matrix.ndim
+    for k, v in axes.items():
+        new[k] = slice(*v)
+    new = tuple(new).copy()
+    return matrix[new]
---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""The Whole Barn """
+
+
+def add_matrices(mat1, mat2):
+    """adds two matrices"""
+    
+    return
+    
