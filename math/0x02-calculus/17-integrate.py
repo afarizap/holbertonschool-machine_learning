7bd4b2ab6e0386618860e3e41ca4e6e2b7f7bfca
afarizap <afarizap@gmail.com>
2021-07-22 07:12:26 -0500

    1st attempt

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,4 +4,10 @@
 
 def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
-    
+    if type(poly) != list or type(C) != int:
+        return None
+    new = []
+    for i in range(1, len(poly) + 1):
+        new += [poly[i-1]/i]
+    new.insert(0, C)
+    return new
