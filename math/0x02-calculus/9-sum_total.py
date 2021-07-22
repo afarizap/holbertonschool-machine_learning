daafdb603ab178b5532a2091d5c36422e43641bb
afarizap <afarizap@gmail.com>
2021-07-21 21:01:53 -0500

    add conditions

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,10 +4,6 @@
 
 def summation_i_squared(n):
     """get the series of a squared sequence"""
-    try:
-        n = int(n)
-        if n < 1:
-            return None
-    except:
+    if type(n) not in [int] or n < 1:
         return None
     return int((n * (n+1) * (2*n + 1)) / 6)
