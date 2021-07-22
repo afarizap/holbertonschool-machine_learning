4a584e170a82228636b76aa5d11884e97078b57c
afarizap <afarizap@gmail.com>
2021-07-22 12:59:51 -0500

    update, now it works

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -15,8 +15,14 @@ u, s, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
 # your code here
-plt.title("PCA of Iris Dataset")
 fig = plt.figure()
-ax3d = fig.add_subplot(111, projection='3d')
-
+ax = fig.add_subplot(111, projection='3d')
+x, y, z = list(zip(*pca_data))
+ax.scatter(x, y, z, c=labels, cmap=plt.get_cmap('plasma'))
+ax.set_title("PCA of Iris Dataset")
+ax.set_xlabel('U1')
+ax.set_ylabel('U2')
+ax.set_zlabel('U3')
 plt.show()
+# https://matplotlib.org/2.0.2/mpl_toolkits/mplot3d/tutorial.html
+# https://matplotlib.org/stable/gallery/color/colormap_reference.html
