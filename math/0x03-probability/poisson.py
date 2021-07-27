8e5274016f5ad5ee2266a3b74db6bb9f575b2873
afarizap <afarizap@gmail.com>
2021-07-27 14:06:10 -0500

    asd

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -10,11 +10,11 @@ class Poisson():
         # self.data = data
         if lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
-        # if data is not None:
+        if data is not None:
         #     # Promedio
         #     lambtha = sum(data)/len(data)
-        #     if type(data) is not list:
-        #         raise TypeError("data must be a list")
-        #     if len(data) < 2:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            #if len(data) < 2:
         #         raise ValueError("data must contain multiple values")
         # self.lambtha = float(lambtha)
