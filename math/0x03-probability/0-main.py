113cd1a793f577be15a949d45559a938a820d9ab
afarizap <afarizap@gmail.com>
2021-07-27 12:36:29 -0500

    add data

--- math/0x03-probability/0-main.py
+++ math/0x03-probability/0-main.py
@@ -11,5 +11,9 @@ print('Lambtha:', p1.lambtha)
 p2 = Poisson(lambtha=5)
 print('Lambtha:', p2.lambtha)
 
+p4 = Poisson({5, 4})
+print('Lambtha:', p4.lambtha)
+
 p3 = Poisson([1])
 print('Lambtha:', p3.lambtha)
+
--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,14 +6,15 @@ class Poisson():
     """represents a poisson distribution"""
     def __init__(self, data=None, lambtha=1.):
         """initialize values"""
-        if data is not None:
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             if type(data) is not list:
                 raise TypeError("data must be a list")
             # Promedio
             lambtha = sum(data)/len(data)
-        else:
-            if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
+        self.data = data
         self.lambtha = float(lambtha)
