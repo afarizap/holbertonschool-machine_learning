0090656ccfe60780d6267d6efa4845a88a522b08
afarizap <afarizap@gmail.com>
2021-07-27 19:42:56 -0500

    initialize exponential

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 """ exponential task """
+
+
+class Exponential:
+    """ represents an exponential distribution """
+
+    def __init__(self, data=None, lambtha=1.):
+        """ class constructor """
+        if lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            # Promedio
+            lambtha = sum(data)/len(data)
+        self.data = data
+        self.lambtha = float(lambtha)
