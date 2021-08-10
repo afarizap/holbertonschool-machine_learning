15d55bfcf7051b3d8f434085cb5ee3d6515d1309
afarizap <afarizap@gmail.com>
2021-08-09 20:44:20 -0500

    add 1 neuron private attributes

--- supervised_learning/0x01-classification/1-neuron.py
+++ supervised_learning/0x01-classification/1-neuron.py
@@ -1,2 +1,40 @@
 #!/usr/bin/env python3
 """ 1-neuron task """
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing a binary
+    classification"""
+
+    def __init__(self, nx):
+        """class constructor
+            args:
+            nx: number of input features to the neuron
+            public instance attributes:
+            w: weights vector for the neuron(random normal dist)
+            b: bias for the neuron(0)
+            A: activated output of the neuron(prediction)(0)
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """W getter"""
+        return self.__W
+
+    @property
+    def b(self):
+        """b getter"""
+        return self.__b
+
+    @property
+    def A(self):
+        """A getter"""
+        return self.__A
