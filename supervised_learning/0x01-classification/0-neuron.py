592aeeb0d683fd66c60715801e547bccbf21231d
afarizap <afarizap@gmail.com>
2021-08-09 20:03:39 -0500

    fix 1st comment

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import numpy as np
 """ 0-neuron task """
+import numpy as np
 
 
 class Neuron:
@@ -11,16 +11,15 @@ class Neuron:
         """class constructor
             args:
             nx: number of input features to the neuron
+            public instance attributes:
+            w: weights vector for the neuron(random normal dist)
+            b: bias for the neuron(0)
+            A: activated output of the neuron(prediction)(0)
         """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        """public instance attributes:
-            w: weights vector for the neuron(random normal dist)
-            b: bias for the neuron(0)
-            A: activated output of the neuron(prediction)(0)
-        """
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
