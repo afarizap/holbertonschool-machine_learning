df09aa17f6db60963cf2a48320b73bb0c662dcf4
afarizap <afarizap@gmail.com>
2021-08-18 03:07:13 -0500

    mean and stdev

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 """ 0-norm_constants task """
+import numpy as np
+
+
+def normalization_constants(X):
+    """calculates the normalization (standardization) constants of a matrix
+        X is the numpy.ndarray of shape (m, nx) to normalize
+            m is the number of data points
+            nx is the number of features
+        Returns: the mean and standard deviation of each feature, respectively
+    """
+    mean = []
+    stdev = []
+    for i in X.T:
+        mean += [i.mean()]
+        stdev += [np.std(i)]
+    return mean, stdev
