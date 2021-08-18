35e6e50fe3dd4b4349c4637d3a937b13b74b322a
afarizap <afarizap@gmail.com>
2021-08-18 11:55:04 -0500

    normalize matrix

--- supervised_learning/0x03-optimization/1-normalize.py
+++ supervised_learning/0x03-optimization/1-normalize.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 """ 1-normalize task """
+import numpy as np
+
+
+def normalize(X, m, s):
+    """normalizes (standardizes) a matrix
+    
+    X is the numpy.ndarray of shape (d, nx) to normalize
+        d is the number of data points
+        nx is the number of features
+    m is a numpy.ndarray of shape (nx,) that contains the mean of all
+     features of X
+    s is a numpy.ndarray of shape (nx,) that contains the standard
+     deviation of all features of X
+    Returns: The normalized X matrix
+
+    """
+    return (X - m) / s
