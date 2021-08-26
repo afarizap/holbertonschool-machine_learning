ae6ba50dfcfbb07bf64209944e801c26911673cb
afarizap <afarizap@gmail.com>
2021-08-25 19:08:25 -0500

    add cost and layer

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """ 2-l2_reg_cost task """
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    """
+    calculates the cost of a neural network with L2 regularization:
+
+        cost is a tensor containing the cost of the network without L2
+         regularization
+        Returns: a tensor containing the cost of the network accounting
+         for L2 regularization
+
+    """
+    return cost + tf.losses.get_regularization_losses()
