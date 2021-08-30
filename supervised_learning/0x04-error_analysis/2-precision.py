47d676cda67a15c2f43f2976246dc0eeb8367c63
afarizap <afarizap@gmail.com>
2021-08-29 23:02:54 -0500

    presicion and specificity

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 """ 2-precision task """
+
+
+def precision(confusion):
+    '''calculates the precision for each class in a confusion matrix
+    Args:
+        confusion is a confusion numpy.ndarray of shape (classes, classes)
+                  where row indices represent the correct labels and column
+                  indices represent the predicted labels
+        classes is the number of classes
+    Returns: a numpy.ndarray of shape (classes,) containing the precision of
+             each class
+    '''
+    True_Positives = np.diagonal(confusion)
+    Predicted_Positives = np.sum(confusion, axis=0)
+    return True_Positives / Predicted_Positives
