fe7d6a764caaba12c72707fff78a11984fa78f6e
afarizap <afarizap@gmail.com>
2021-08-30 19:06:18 -0500

    add sequential with 3 dropout

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python3
 """ 0-sequential task """
+import tensorflow.keras as K
+from tensorflow.python.keras.backend import dropout
+from tensorflow.python.keras.layers.core import Dropout
+from tensorflow.python.keras.regularizers import Regularizer
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    builds a neural network with the Keras library:
+
+    nx: is the number of input features to the network
+    layers: is a list containing the number of nodes in each layer of the network
+    activations: is a list containing the activation functions used for each layer of the network
+    lambtha: is the L2 regularization parameter
+    keep_prob: is the probability that a node will be kept for dropout
+    Returns: the keras model
+
+    """
+    model = K.Sequential()
+    model.add(K.Input(shape=(nx,)))
+    for i in range(len(layers)):
+        model.add(K.layers.Dense(units=layers[i],
+                  activation=activations[i],
+                  activity_regularizer=K.regularizers.L2(lambtha)),
+                  K.layers.Dropout(keep_prob))
+    return model
--- supervised_learning/0x06-keras/README.md
+++ supervised_learning/0x06-keras/README.md
@@ -1,1 +1,31 @@
-Hello README
+# General
+
+    - What is Keras?
+
+        - Keras is a deep learning API written in Python, running on top of the machine learning platform TensorFlow. It was developed with a focus on enabling fast experimentation.
+
+    - What is a model?
+
+        - Keras model represents the actual neural network model.
+
+    - How to instantiate a model (2 ways)
+
+        - With Secuential API and Functional API
+        
+        [keras_models](https://www.tutorialspoint.com/keras/keras_models.htm)
+
+    - How to build a layer
+    - How to add regularization to a layer
+    - How to add dropout to a layer
+    - How to add batch normalization
+    - How to compile a model
+    - How to optimize a model
+    - How to fit a model
+    - How to use validation data
+    - How to perform early stopping
+    - How to measure accuracy
+    - How to evaluate a model
+    - How to make a prediction with a model
+    - How to access the weights/outputs of a model
+    - What is HDF5?
+    - How to save and load a model’s weights, a model’s configuration, and the entire model
