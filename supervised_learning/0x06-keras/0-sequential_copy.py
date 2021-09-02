15634841258608f0ff262ae8078f448f0a8cc1e3
afarizap <afarizap@gmail.com>
2021-09-01 20:11:41 -0500

    fix

---
+++ supervised_learning/0x06-keras/0-sequential_copy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" 0-sequential task """
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    builds a neural network with the Keras library:
+
+    nx: is the number of input features to the network
+    layers: is a list containing the number of nodes in each layer of the network
+    activations: is a list containing the activation functions used for each layer of the network
+    lambtha: is the L2 regularization parameter
+    keep_prob: is the probability that a node will be kept for dropout
+    Returns: the keras model
+
+    """
+    model = K.Sequential()
+    model.add(K.Input(shape=(nx,)))
+    for i in range(len(layers)):
+        model.add(K.layers.Dense(units=layers[i],
+                  activation=activations[i],
+                  activity_regularizer=K.regularizers.L2(lambtha)))
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(keep_prob))
+    return model
